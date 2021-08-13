d6e1c369cacbd88720bf8688fb3051c100051f6f
Nate <n621200n621200@gmail.com>
2021-08-13 15:19:57 +0800

    add MainActivity

--- app/src/main/java/com/practice/astronomy/view/MainActivity.kt
+++ app/src/main/java/com/practice/astronomy/view/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.practice.astronomy.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.practice.astronomy.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestTextView: TextView by lazy { findViewById<TextView>(R.id.tvRequest) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestTextView.setOnClickListener {
+            startActivity(Intent(this,ListAstronomyActivity::class.java))
+        }
+
     }
 }
